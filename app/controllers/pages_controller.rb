d265300f66b9c8fad2b2070b63a6cc48515be563
Toomas Arumae <tom.arumae@gmail.com>
2021-04-15 20:03:30 +0100

    Fixed the API call, fixed the routes and pages. Pulling data from API and sorting it works as intended

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,15 +4,14 @@ require 'json'
 URL = "https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=XXXXXXXXXXXXXXXXX&steamid=76561197960434622&format=json"
 
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home, :steam_call ]
+  skip_before_action :authenticate_user!, only: [ :home, :steam_call, :steam_request ]
 
-  def home
-  end
+  def home;end
 
-  def steam_call
+  def steam_request
     steam_id = params[:steam_id][:steam_id]
     response = JSON.parse(open("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=#{ENV['STEAM_API']}&steamid=#{steam_id}&include_appinfo=true&include_played_free_games=true&format=json").read)
-    parsed_response = response["response"]["games"].sort_by{ |game| game["playtime_forever"] }.reverse.first(10)
-    raise
+    @sorted_games = response["response"]["games"].sort_by{ |game| game["playtime_forever"] }.reverse.first(10)
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  get '/request', to: 'pages#request', as: 'request'
-  post '/request', to: 'pages#steam_call', as: 'steamcall'
+  post '/steam_request', to: 'pages#steam_request', as: 'steamrequest'
 end
